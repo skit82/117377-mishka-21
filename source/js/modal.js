0b52da248c159f665e23056ee6677c59d45582ce
Aleksey <a.abrosimow2012@yandex.ru>
2021-01-09 15:39:05 +0300

    Адаптивная графика для страницы каталога

---
+++ source/js/modal.js
@@ -0,0 +1,14 @@
+for (var btn__order = document.querySelectorAll(".btn__order"), popup = document.querySelector(".modal__order"), overlay = document.querySelector(".modal__overlay"), i = 0; i < btn__order.length; i++)
+btn__order[i].addEventListener("click", function(e) {
+  e.preventDefault(),
+  popup.classList.add("modal__order--show"),
+  overlay.classList.add("modal__overlay--show")
+}),
+overlay.addEventListener("click", function(e) {
+  popup.classList.remove("modal__order--show"),
+  overlay.classList.remove("modal__overlay--show")
+}),
+window.addEventListener("keydown", function(e) {
+  27 === e.keyCode && popup.classList.contains("modal__order--show") && (popup.classList.remove("modal__order--show"),
+  overlay.classList.remove("modal__overlay--show"))
+});
